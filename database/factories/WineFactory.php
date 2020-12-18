e30a34bb3256be6e9799b971c07630bd97a4bd7e
Luis Cavalari <luiscavalari@ufpr.br>
2020-12-18 14:39:32 -0300

    refactor: clean menu  event function assigment

---
+++ database/factories/WineFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Wine;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WineFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Wine::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->text(200),
+            'grade' => rand(0,10)
+        ];
+    }
+}
